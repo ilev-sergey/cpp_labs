9f0daf33c0accd3a1f779edbaf596b0276151efd
Sergey Ilev <ilev.sv@phystech.edu>
2021-10-07 22:25:41 +0300

    Problem3 reworked

--- Task5/Problem3.cpp
+++ Task5/Problem3.cpp
@@ -1,20 +1,58 @@
-﻿class Class
+﻿#include <iostream>
+
+class MyClass;          // класс, к экземплярам которого требуется получить доступ
+class AccessProvider;   // промежуточный класс, который обеспечивает предоставление доступа
+void function(MyClass&); // функция, которая должна получить ограниченный доступ к MyClass
+
+
+class MyClass
 {
 private:
-    int date1;
-protected:
-    int date2;
-    friend f1(); // access to date1, date2
+    int m_x;
+    int m_y;
+
+public:
+    MyClass(int x = 0, int y = 0) : m_x{ x }, m_y{ y } {}
+    friend AccessProvider;
 };
 
-class ChildClass : private
+class AccessProvider
 {
-    // private date2
 private:
-    int date3;
-    friend f2(); // access to date2, date3
+    MyClass _object;
+    AccessProvider(MyClass& object) : _object{ object } {};
+
+    int getX() const
+    {
+        return _object.m_x;
+    }
+
+    friend void function(MyClass&);
+};
+
+void function(MyClass& object)
+{
+    AccessProvider provider(object);                // создаём экземпляр класса-провайдера
+                                                    // с данными экземпляра object
+    std::cout << "x = " << provider.getX() << '\n'; // функция может получать значение поля "x"
 }
 
-// date1 is available for f1
-// date2 is available for f1, f2
-// date3 is available for f2
+int main()
+{
+    std::cout << "Enter x, y:\n";
+    int x;
+    std::cin >> x;
+    int y;
+    std::cin >> y;
+
+    MyClass object{ x, y }; // создаём экземпляр класса, с которым мы работаем
+    function(object);       // передаём экземпляр в функцию
+}
+
+// function имеет ограниченный доступ к полю "x" экземпляра класса MyClass:
+// может получить значение, но не изменить его.
+// При этом function не имеет доступа к остальным полям ("y").
+// 
+// При необходимости добавления функций необходимо создать свой класс-провайдер для каждой функции,
+// в котором необходимо объявить функцию другом и определить необходимые геттеры/сеттеры для данных,
+// к которым требуется доступ.
